e06e9f894b474502ad543fedca2ef46dfb970b1c
jungpig <pig.ohgiraffers@gmail.com>
2024-12-05 12:35:41 +0900

    test

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,11 +1,33 @@
-
+import React, { useState, useEffect } from 'react';
 
 const Menupage = () => {
-    return (
-        <>
-            <h1>우리가 메뉴를 불러와서 보여줄 페이지</h1>
-        </>
-    )
-}
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.jungpigapp.shop:9074/menu/select')
+      .then((response) => response.json())
+      .then((data) => setMenu(data))
+      .catch((error) => console.error('Error fetching menu:', error));
+  }, []);
+
+  return (
+    <>
+      <h1>우리가 메뉴를 불러와서 보여줄 페이지</h1>
+      <ul>
+        {menu.length > 0 ? (
+          menu.map((item, index) => (
+            <li key={index}>
+              <h2>{item.menuName}</h2>
+              <p>{item.menuPrice}</p>
+              <p>Price: {item.categoryCode}</p>
+            </li>
+          ))
+        ) : (
+          <p>메뉴를 불러오는 중...</p>
+        )}
+      </ul>
+    </>
+  );
+};
 
 export default Menupage;
